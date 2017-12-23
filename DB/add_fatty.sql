0cd9ec2cd5eef10659499f6f06bce559e230cd3d
lolialone <Lolialone>
2017-12-24 02:20:00 +0300

    add stored procs

---
+++ DB/add_fatty.sql
@@ -0,0 +1,48 @@
+﻿CREATE OR REPLACE FUNCTION add_fatty(
+  name text,
+  surname text,
+  password text,
+  email text,
+  birthday timestamp without time zone,
+  sex smallint,
+  hidefoodtrach boolean,
+  hideage boolean,
+  hideemail boolean,
+  currentweight decimal,
+  desiredweight decimal,
+  heigth decimal,
+  trainer_id integer
+) 
+RETURNS INTEGER AS $$
+BEGIN
+  INSERT INTO public.fattyes(
+    name,
+    surname,
+    password,
+    email,
+    birthday,
+    sex,
+    hidefoodtrach,
+    hideage,
+    hideemail,
+    currentweight,
+    desiredweight,
+    heigth,
+    trainer_id)
+  VALUES(
+    name,
+    surname,
+    password,
+    email,
+    birthday,
+    sex,
+    hidefoodtrach,
+    hideage,
+    hideemail,
+    currentweight,
+    desiredweight,
+    heigth,
+    trainer_id) RETURNING id;
+	
+END;
+$$ LANGUAGE plpgsql;
---
+++ DB/get_fatty_by_id.sql
@@ -0,0 +1,40 @@
+﻿CREATE OR REPLACE FUNCTION get_fatty_by_id(
+  fatty_id text
+) 
+RETURNS TABLE
+(
+  id integer,
+  name text,
+  surname text,
+  password text,
+  email text,
+  birthday timestamp without time zone,
+  sex smallint,
+  hidefoodtrach boolean,
+  hideage boolean,
+  hideemail boolean,
+  currentweight decimal,
+  desiredweight decimal,
+  heigth decimal,
+  trainer_id integer
+)
+ AS $$
+BEGIN
+  RETURN QUERY SELECT(
+    f.name,
+    f.surname,
+    f.password,
+    f.email,
+    f.birthday,
+    f.sex,
+    f.hidefoodtrach,
+    f.hideage,
+    f.hideemail,
+    f.currentweight,
+    f.desiredweight,
+    f.heigth,
+    f.trainer_id)
+  FROM public.fattyes f
+  WHERE f.id = fatty_id;
+END;
+$$ LANGUAGE plpgsql;

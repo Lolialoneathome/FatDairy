9f1d787d9e564c7fa283539e2677809c856e93af
lolialone <Lolialone>
2017-12-24 02:19:24 +0300

    add fatty repo interface

---
+++ Domain/Repos/IFattyRepository.cs
@@ -0,0 +1,14 @@
+﻿using FatDairy.Domain.Models;
+using System.Threading.Tasks;
+
+namespace FatDairy.Domain.Repos
+{
+    public interface IFattyRepository
+    {
+        Task AddFatty(Fatty fatty);
+
+        Task<Fatty> GetById(int id);
+
+        Task<Fatty> GetByEmail(string email);
+    }
+}

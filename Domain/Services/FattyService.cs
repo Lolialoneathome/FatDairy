805ad5ec240a808efeadef34b68cf7f2466352d3
lolialone <Lolialone>
2017-12-24 02:19:39 +0300

    add fatty service

---
+++ Domain/Services/FattyService.cs
@@ -0,0 +1,41 @@
+﻿using FatDairy.Domain.Models;
+using FatDairy.Domain.Repos;
+using System;
+using System.Threading.Tasks;
+
+namespace FatDairy.Domain.Services
+{
+    public class FattyService
+    {
+        protected readonly IFattyRepository _fattyRepository;
+
+        public async Task<Fatty> AddFattyAsync(UserInfo userInfo,
+            bool hideFoodTrack,
+            bool hideAge,
+            bool hideEmail,
+            double currentWeigth,
+            double desiredWeigth,
+            double height,
+            int? trainerId = null)
+        {
+            var maybeExistFatty = _fattyRepository.GetByEmail(userInfo.Email);
+            if (maybeExistFatty != null)
+                throw new InvalidOperationException("Fatty with this email already exist");
+
+            var newFatty = new Fatty(
+                userInfo,
+                hideFoodTrack,
+                hideAge,
+                hideEmail,
+                currentWeigth,
+                desiredWeigth,
+                height,
+                null
+                );
+
+            await _fattyRepository.AddFatty(newFatty);
+
+            return newFatty;
+        }
+    }
+}
